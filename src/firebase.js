0f54a6b312ead81b7cb1d6dd4f7e03220b8bee0f
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2023-04-25 09:33:20 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7CzvmTmlC-ByOx1w5NT5hLt1EuUAePus",
-    authDomain: "fir-todo-ddbf6.firebaseapp.com",
-    projectId: "fir-todo-ddbf6",
-    storageBucket: "fir-todo-ddbf6.appspot.com",
-    messagingSenderId: "181833437727",
-    appId: "1:181833437727:web:b633620e29765365c57b2a"
+
 };
 
 const app = initializeApp(firebaseConfig);
